5091bf8acc5187af36fe322d81ce411a8211d180
robdodson <robdodson@google.com>
2022-01-15 14:29:45 -0800

    Add 3x3 check

--- src/App.js
+++ src/App.js
@@ -28,15 +28,15 @@ function getCell(grid, x, y) {
 function checkRows(grid) {
   let isValid = true;
   for (let y = 0; y < grid.rows; y++) {
-    const numbers = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    const numbers = new Set();
     for (let x = 0; x < grid.cols; x++) {
       const cell = getCell(grid, x, y);
-      if (!numbers.has(cell)) {
+      if (numbers.has(cell)) {
         isValid = false;
         break;
       }
 
-      numbers.delete(cell);
+      numbers.add(cell);
     }
   }
   return isValid;
@@ -45,23 +45,42 @@ function checkRows(grid) {
 function checkColumns(grid) {
   let isValid = true;
   for (let x = 0; x < grid.cols; x++) {
-    const numbers = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    const numbers = new Set();
     for (let y = 0; y < grid.rows; y++) {
       const cell = getCell(grid, x, y);
-      if (!numbers.has(cell)) {
+      if (numbers.has(cell)) {
         isValid = false;
         break;
       }
 
-      numbers.delete(cell);
+      numbers.add(cell);
+    }
+  }
+  return isValid;
+}
+
+function checkGrids(grid) {
+  let isValid = true;
+  for (let y = 0; y < grid.rows; y += 3) {
+    for (let x = 0; x < grid.cols; x += 3) {
+      const numbers = new Set();
+      for (let i = 0; i < 9; i++) {
+        let newX = x + (i % 3);
+        let newY = y + Math.floor(i / 3);
+        const cell = getCell(grid, newX, newY);
+        if (numbers.has(cell)) {
+          isValid = false;
+          break;
+        }
+        numbers.add(cell);
+      }
     }
   }
   return isValid;
 }
 
 function App() {
-  const isValid = checkRows(grid) && checkColumns(grid);
-  console.log(isValid);
+  const isValid = checkGrids(grid) && checkRows(grid) && checkColumns(grid);
   return (
     <div className="App">
       <table>
